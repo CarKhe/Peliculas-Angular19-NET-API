bea6adc14251163b572c72d945c8e3140bd4f09e
Carlos Enrique Rodríguez Morán <95141637+CarKhe@users.noreply.github.com>
2025-06-16 18:29:10 -0500

    Interfaces, Tiempos de Vida y Validaciones

---
+++ IRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI
+{
+    public interface IRepositorio
+    {
+        List<Genero> GetAllGeneros();
+        Task<Genero?> GetOne(int id);
+
+        bool IsExist(string nombre);
+    }
+}
---
+++ RepositorioSQL.cs
@@ -0,0 +1,22 @@
+﻿using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI
+{
+    public class RepositorioSQL : IRepositorio
+    {
+        public List<Genero> GetAllGeneros()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Genero?> GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsExist(string nombre)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
